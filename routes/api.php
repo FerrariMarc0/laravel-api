4b5adf31011e18b643c04ace40da6cbb40669589
FerrariMarc0 <marco1010@live.it>
2023-05-29 19:53:12 +0200

    link

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\PortfolioController;
+use App\Http\Controllers\Api\TypeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,3 +22,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('projects', [PortfolioController::class, 'index']);
 Route::get('projects/{slug}', [PortfolioController::class, 'show']);
+Route::get('types', [TypeController::class, 'index']);
+Route::get('types/{slug}', [TypeController::class, 'show']);
